0b98c543a21b0fd13092c74af2d7dbda0026f870
SH4D <80900735+Parvxi@users.noreply.github.com>
2023-11-23 18:25:09 +0300

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,8 @@
+<footer>
+    <!--**bottom**********************-->
+    <div class="footer-bottom">
+      <!--**  <span>Made By Sukoon Team</span> -->
+        <span>&copy; Sukoon <?php echo date("Y"); ?></span>
+    </div>
+</footer>
+
---
+++ header.php
@@ -0,0 +1,13 @@
+<!-- header.php -->
+
+<!DOCTYPE html>
+
+
+<header style="position: fixed; top: 10px; right: 10px;">
+        <a href="../pages/index.php" class="logo">
+            <img src="../images/fav-icon.png" width="50" height="50" alt="Sukoon Logo">
+        </a>
+    </header>
+
+<!-- end of header.php -->
+
