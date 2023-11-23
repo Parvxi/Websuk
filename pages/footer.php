5bf5e5fb130f0ad5c5c0b526a3a7efdc7915096f
SH4D <80900735+Parvxi@users.noreply.github.com>
2023-11-23 18:24:54 +0300

    Delete pages directory

--- pages/footer.php
+++
@@ -1,8 +0,0 @@
-<footer>
-    <!--**bottom**********************-->
-    <div class="footer-bottom">
-      <!--**  <span>Made By Sukoon Team</span> -->
-        <span>&copy; Sukoon <?php echo date("Y"); ?></span>
-    </div>
-</footer>
-
